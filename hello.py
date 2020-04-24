0399260a57fedb3d967a94020fc4e70cb8a52e3d
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-24 01:45:53 -0700

    checkout 5b

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
-from flask import Flask,render_template,flash,session,redirect,url_for
+from flask import Flask,render_template,flash,session,redirect,url_for,request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 import os
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 basedir=os.path.abspath(os.path.dirname(__file__))
 
 app=Flask(__name__)
@@ -15,23 +17,47 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db=SQLAlchemy(app)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
+
+class NameForm(FlaskForm):
+    name=StringField('What is your name?', validators=[DataRequired()])#it isan html input elem with type="text"
+    submit=SubmitField('Submit')#it is basically an html input elem with type="submit" attr
+
+class Role(db.Model):
+    __tablename__='roles'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(64),unique=True)
+    users=db.relationship('User',backref='role',lazy='dynamic')
+    def __repr__(self):
+        return '<Role %r>' % self.name
+class User(db.Model):
+    __tablename__='users'
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(64),unique=True,index=True)
+    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
 # @app.route("/")
 # def index():
 #     return "<h1>Hello World!</h1>"
 
-from flask import request
 @app.route("/",methods=["GET","POST"])
 def index():#request enables to access globally certain obj without adding an arg to view func
     form=NameForm()
-    
     if form.validate_on_submit():
-        old_name=session.get('name')
-        if old_name is not None and old_name!=form.name.data:
-            flash('Looks like you have changed your name')
-
+        user=User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user=User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known']=False
+        else:
+            session['known']=True
         session['name']=form.name.data
+        form.name.data=""
         return redirect(url_for('index'))
-    return render_template('index.html',form=form,name=session.get('name'))
+    return render_template('index.html',form=form,name=session.get('name'),known=session.get('known',False))
     #current_time=datetime.utcnow()
 
 # @app.route("/")
@@ -60,27 +86,4 @@ def index():#request enables to access globally certain obj without adding an ar
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html",name=name)
-
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired
-
-class NameForm(FlaskForm):
-    name=StringField('What is your name?', validators=[DataRequired()])#it isan html input elem with type="text"
-    submit=SubmitField('Submit')#it is basically an html input elem with type="submit" attr
-
-class Role(db.Model):
-    __tablename__='roles'
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(64),unique=True)
-    users=db.relationship('User',backref='role')
-    def __repr__(self):
-        return '<Role %r>' % self.name
-class User(db.Model):
-    __tablename__='users'
-    id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.String(64),unique=True,index=True)
-    role_id=db.Column(db.Integer,db.ForeignKey('role.id'))
-    def __repr__(self):
-        return '<User %r>' % self.username
 
