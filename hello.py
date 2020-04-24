64192283bb9fe019dc319311b174b841f617276b
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-23 19:21:45 -0700

    end of chapter 2

--- hello.py
+++ hello.py
@@ -12,6 +12,29 @@ def index():#request enables to access globally certain obj without adding an ar
     user_agent=request.headers.get('User-Agent')
     return '<p>Your browser is {}</p>'.format(user_agent)
 
+# @app.route("/")
+# def index():
+#     return '<h1>Bad Request</h1>',400
+# from flask import make_response
+
+# @app.route("/")
+# def index():
+#     response=make_response("<h1>This document carries a cookies!</h1>")
+#     response.set_cookie('answer','42')
+#     return response
+
+# @app.route("/")
+# def index():
+#     return redirect("http://www.example.com")
+
+# from flask import abort
+# @app.route("/user/<id>")
+# def get_user(id):
+#     user=load_user(id)
+#     if not user:
+#         abort(404)
+#     return '<h1>Hello, {}</h1>'.format(user.name)
+
 @app.route("/user/<name>")
 def user(name):
     return "<h1>Hello, {}!</h1>".format(name)
