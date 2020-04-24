434d0181bc1c4a5b6f61aea346b2e64bf7928980
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-23 22:57:42 -0700

    correspond to checkout 4b

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session,redirect,url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+import os
 app=Flask(__name__)
+app.config["SECRET_KEY"]=os.environ["wtfKey"]
 bootstrap=Bootstrap(app)
 moment=Moment(app)
 # @app.route("/")
@@ -10,9 +12,14 @@ moment=Moment(app)
 #     return "<h1>Hello World!</h1>"
 
 from flask import request
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def index():#request enables to access globally certain obj without adding an arg to view func
-    return render_template('index.html',current_time=datetime.utcnow())
+    form=NameForm()
+    if form.validate_on_submit():
+        session['name']=form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form,name=session.get('name'))
+    #current_time=datetime.utcnow()
 
 # @app.route("/")
 # def index():
@@ -40,3 +47,11 @@ def index():#request enables to access globally certain obj without adding an ar
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html",name=name)
+
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name=StringField('What is your name?', validators=[DataRequired()])#it isan html input elem with type="text"
+    submit=SubmitField('Submit')#it is basically an html input elem with type="submit" attr
