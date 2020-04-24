9cd3852a183f5671c82db7ecf65a573e21c2ae2e
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-24 11:26:42 -0700

    successfully installed email sending system @6b

--- hello.py
+++ hello.py
@@ -6,9 +6,10 @@ from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail,Message
 from datetime import datetime
 import os
-
+from threading import Thread
 basedir=os.path.abspath(os.path.dirname(__file__))
 
 app=Flask(__name__)
@@ -16,12 +17,22 @@ app.config["SECRET_KEY"]=os.environ["wtfKey"]
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT']=587
+app.config['MAIL_USE_TLS']=True
+app.config['MAIL_USERNAME']='obeythetestinggoat2@gmail.com'
+app.config['MAIL_PASSWORD']=os.environ.get("PW")
+app.config['FLASKY_MAIL_SUBJECT_PREFIX']='[FLASKY]'
+app.config['FLASKY_MAIL_SENDER']='FLASKY Admin <flasky@example.com>'
+app.config['FLASKY_ADMIN']=os.environ.get('FLASKY_ADMIN')#recepient
+
 db=SQLAlchemy(app)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
 
 migrate=Migrate(app,db)
-
+mail=Mail(app)
 class NameForm(FlaskForm):
     name=StringField('What is your name?', validators=[DataRequired()])#it isan html input elem with type="text"
     submit=SubmitField('Submit')#it is basically an html input elem with type="submit" attr
@@ -41,7 +52,17 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-
+def send_email(to,subject,template,**kwargs):#kwargs will be template context, see line 3,4 below
+    msg=Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX']+subject,sender=app.config['FLASKY_MAIL_SENDER'],recipients=[to])
+    
+    msg.body=render_template(template+'.txt',**kwargs)
+    msg.html=render_template(template+'.html',**kwargs)
+    thr=Thread(target=send_async_email,args=[app,msg])
+    thr.start()
+    return thr    
+def send_async_email(app,msg):
+    with app.app_context():
+        mail.send(msg)
 # @app.route("/")
 # def index():
 #     return "<h1>Hello World!</h1>"
@@ -56,6 +77,8 @@ def index():#request enables to access globally certain obj without adding an ar
             db.session.add(user)
             db.session.commit()
             session['known']=False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'],'New User','mail/new_user',user=user)
         else:
             session['known']=True
         session['name']=form.name.data
@@ -93,3 +116,4 @@ def user(name):
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db,User=User,Role=Role)
+
