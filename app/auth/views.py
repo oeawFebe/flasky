28daba5992e33f0d5537cca0c76bc8fa1fbb867f
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-24 20:05:11 -0700

    checkout 8d

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,8 +2,8 @@ from flask import render_template,redirect,request,url_for,flash
 from flask_login import login_user, logout_user, login_required,current_user
 from . import auth
 from ..models import User
-from .forms import LoginForm
-
+from .forms import LoginForm,RegistrationForm
+from .. import db
 @auth.route('/login',methods=["GET","POST"])
 def login():
     form=LoginForm()
@@ -30,3 +30,15 @@ def logout():
 def secret():
     return "Only authenticated users are allowed!"
 
+@auth.route('/register',methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    if form.validate_on_submit():
+        user=User(email=form.email.data,
+            username=form.username.data,
+            password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html',form=form)
