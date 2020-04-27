bd7399447097e8027adb31e266c2e114535ee19d
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-27 15:30:22 -0700

    8h sendemail still not working

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,8 @@ from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import User
-from .forms import LoginForm, RegistrationForm,ChangePasswordForm,PasswordResetRequestForm,PasswordResetForm
+from .forms import (LoginForm, RegistrationForm,ChangePasswordForm,
+    PasswordResetRequestForm,PasswordResetForm,ChangeEmailForm)
 from .. import db
 from ..email import send_email
 
@@ -119,7 +120,7 @@ def password_reset_request():
     return render_template('auth/reset_password.html',form=form)
 
 @auth.route('/reset/<token>',methods=['GET','POST'])
-def password_rest(token):
+def password_reset(token):
     if not current_user.is_anonymous:
         return redirect(url_for('main.index'))
     form=PasswordResetForm()
@@ -131,3 +132,29 @@ def password_rest(token):
         else:
             return redirect(url_for('main.index'))
     return render_template('auth/reset_password.html',form=form)
+
+@auth.route("/change_email",methods=["POST","GET"])
+@login_required
+def change_email_request():
+    form=ChangeEmailForm()
+    if form.validate_on_submit():
+        if current_user.verify_password(form.password.data):
+            new_email=form.email.data.lower()
+            token=current_user.generate_email_change_token(new_email)
+            send_email(new_email,"Confirm your email address",
+                "auth/email/change_email",user=current_user,token=token)
+            flash("An email with instructions to confirm address has been sent to you.")
+            return redirect(url_for("main.index"))
+        else:
+            flash("Invalid email or password")
+    return render_template("auth/change_email.html",form=form)
+
+@auth.route("/change_email/<token>")
+@login_required
+def change_email(token):
+    if current_user.change_email(token):
+        db.session.commt()
+        flash("your email address has been updated.")
+    else:
+        flash("Invalid request.")
+    return redirect(url_for('main.index'))
--- app/models.py
+++ app/models.py
@@ -53,7 +53,7 @@ class User(UserMixin, db.Model):
 
     def generate_reset_token(self,expiration=3600):
         s=Serializer(current_app.config['SECRET_KEY'],expiration)
-        return s.dump({'reset':self.id}).decode('utf-8')
+        return s.dumps({'reset':self.id}).decode('utf-8')
 
     @staticmethod
     def reset_password(token,new_password):
@@ -68,12 +68,32 @@ class User(UserMixin, db.Model):
         user.password=new_password
         db.session.add(user)
         return True
-
-
+    def generate_email_change_token(self,new_email,expiration=3600):
+        s=Serializer(current_app.config["SECRET_KEY"],expiration)
+        return s.dumps(
+            {"change_email":self.id,"new_email":new_email}).decode("utf-8")
+    def change_email(self,token):
+        s=Serializer(current_app.config["SECRET_KEY"])
+        try:
+            data=s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get("change_email")!=self.id:
+            return False
+        new_email=data.get("new_email")
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email):
+            return False
+        self.email=new_email
+        db.session.add(self)
+        return True
     def __repr__(self):
         return '<User %r>' % self.username
 
-
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
