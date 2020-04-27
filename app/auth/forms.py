c818b5108cdb956b5b2de58dd646bc0a2bc01d30
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-27 03:00:05 -0700

    checkout 8f

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,32 +1,48 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import DataRequired, Length,Email,Regexp,EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 from ..models import User
 
+
 class LoginForm(FlaskForm):
-    email=StringField("Email",validators=[DataRequired(),Length(1,64),Email()])
-    password=PasswordField("Password",validators=[DataRequired()])
-    remember_me=BooleanField("Keep me logged in")
-    submit=SubmitField("Log In")
+    email = StringField("Email", validators=[
+                        DataRequired(), Length(1, 64), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Keep me logged in")
+    submit = SubmitField("Log In")
+
 
 class RegistrationForm(FlaskForm):
-    email=StringField("Email",validators=[DataRequired(),
-        Length(1,64),
-        Email()
-        ])
-    username=StringField('Username',validators=[
+    email = StringField("Email", validators=[DataRequired(),
+                                             Length(1, 64),
+                                             Email()
+                                             ])
+    username = StringField('Username', validators=[
         DataRequired(),
-        Length(1,64),
-        Regexp('^[A-Za-z][A-Za-z0-9_.]*$',0,'Username must have only letters, numbers, dots or underscores')
-        ])
-    password=PasswordField("Password",validators=[DataRequired(),EqualTo('password2',message='Passwords must match.')])
-    password2=PasswordField('Confirm password',validators=[DataRequired()])
-    submit=SubmitField("Regiter")
-def validate_email(self,field):
-    if User.query.filter_by(email=field.data).first():
-        raise ValidationError('Email already registered.')
-def validate_username(self,field):
-    if User.query.filter_by(username=field.data).first():
-        raise ValidationError('Username already in use.')
-#validate_* automatically added to field named "*"
+        Length(1, 64),
+        Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+               'Username must have only letters, numbers, dots or underscores')
+    ])
+    password = PasswordField("Password", validators=[DataRequired(), EqualTo(
+        'password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField("Regiter")
+
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
+    # validate_* automatically added to field named "*"
+
+class ChangePasswordForm(FlaskForm):
+    old_password=PasswordField('Old password',validators=[DataRequired()])
+    password=PasswordField('New Password',validators=[
+        DataRequired(),EqualTo('password2',message='Password must match')])
+    password2=PasswordField("Confirm new password",validators=[DataRequired()])
+    submit=SubmitField('Update Password')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,22 +1,26 @@
-from flask import render_template,redirect,request,url_for,flash
-from flask_login import login_user, logout_user, login_required,current_user
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from ..models import User
-from .forms import LoginForm,RegistrationForm
+from .forms import LoginForm, RegistrationForm,ChangePasswordForm
 from .. import db
-@auth.route('/login',methods=["GET","POST"])
+from ..email import send_email
+
+
+@auth.route('/login', methods=["GET", "POST"])
 def login():
-    form=LoginForm()
+    form = LoginForm()
     if form.validate_on_submit():
-        user=User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
-            login_user(user,form.remember_me.data)
-            next=request.args.get('next')
+            login_user(user, form.remember_me.data)
+            next = request.args.get('next')
             if next is None or not next.startswith("/"):
-                next=url_for("main.index")
+                next = url_for("main.index")
             return redirect(next)
         flash("Invalid username or password.")
-    return render_template('auth/login.html',form=form)
+    return render_template('auth/login.html', form=form)
+
 
 @auth.route("/logout")
 @login_required
@@ -25,20 +29,76 @@ def logout():
     flash("You have been logged out.")
     return redirect(url_for('main.index'))
 
+
 @auth.route('/secret')
 @login_required
 def secret():
     return "Only authenticated users are allowed!"
 
-@auth.route('/register',methods=['GET','POST'])
+
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
-    form=RegistrationForm()
+    form = RegistrationForm()
     if form.validate_on_submit():
-        user=User(email=form.email.data,
-            username=form.username.data,
-            password=form.password.data)
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('You can now login.')
-        return redirect(url_for('auth.login'))
-    return render_template('auth/register.html',form=form)
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account',
+                   'auth/email/confirm', user=user, token=token)
+        flash("A confirmation email has been sent to you by email.")
+        return redirect(url_for('main.index'))
+    return render_template('auth/register.html', form=form)
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        db.session.commit()
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated and not current_user.confirmed and request.blueprint != "auth" and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirma Your Account',
+               'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been sent to you by email.')
+    return redirect(url_for('main.index'))
+
+@auth.route('/change_password',methods=["GET","POST"])
+@login_required
+def change_password():
+    form=ChangePasswordForm()
+    if form.validate_on_submit():
+        if current_user.verify_password(form.old_password.data):
+            current_user.password=form.password.data
+            db.session.add(current_user)
+            db.session.commit()
+            flash("Your password has been updated")
+            return redirect(url_for('main.index'))
+        else:
+            flash("Invalid password")
+    return render_template('auth/change_password.html',form=form)
