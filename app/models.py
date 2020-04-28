300b8efa21f933718f2ffee7a1e1ccb8bcdab231
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-27 20:00:15 -0700

    end of chapter 10

--- app/models.py
+++ app/models.py
@@ -68,7 +68,7 @@ class User(UserMixin, db.Model):
     about_me=db.Column(db.Text())
     member_since=db.Column(db.DateTime(),default=datetime.utcnow)#function can be given. will be invoked bySQLAlchemy
     last_seen=db.Column(db.DateTime(),default=datetime.utcnow)
-
+    avatar_hash=db.Column(db.String(32))
     def __init__(self,**kwargs):
         super(User,self).__init__(**kwargs)
         if self.role is None:
@@ -76,6 +76,8 @@ class User(UserMixin, db.Model):
                 self.role=Role.query.filter_by(name="Administrator").first()
             if self.role is None:
                 self.role=Role.query.filter_by(default=True).first()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash=self.gravatar_hash()
     @property
     def password(self):
         raise AttributeError("password is not a readable attribute")
@@ -142,16 +144,22 @@ class User(UserMixin, db.Model):
         if self.query.filter_by(email=new_email) is not None:
             return False
         self.email=new_email
+        self.avatar_hash=self.gravatar_hash()
         db.session.add(self)
         return True
+    def gravatar_hash(self):
+        return hashlib.md5(self.email.lower().encode("utf-8")).hexdigest()
     
     def can(self,perm):
         return self.role is not None and self.role.has_permission(perm)
     def is_administrator(self):
         return self.can(Permission.ADMIN)
     def gravatar(self,size=100,default='identicon',rating='g'):
-        url="https://secure.gravatar.com/avatar"
-        hash=hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        if request.is_secure:
+            url="https://secure.gravatar.com/avatar"
+        else:
+            url="https://www.gravatar.com/avatar"
+            hash=self.avatar_hash or self.gravatar_hash()            
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
             url=url,hash=hash,size=size,default=default,rating=rating)
 
