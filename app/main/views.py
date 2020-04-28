61dc1a7ac5d60fa3d0efd3ee8cea58c2c6e9fe4e
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-28 01:26:53 -0700

    12b

--- app/main/views.py
+++ app/main/views.py
@@ -6,7 +6,7 @@ from .. import db
 from ..models import Permission,Role,User,Post
 from ..email import send_email
 from flask_login import login_required, current_user
-from ..decorators import admin_required
+from ..decorators import admin_required,permission_required
 from .forms import EditProfileForm, EditProfileAdminForm
 
 @main.route('/edit-profile/<int:id>',methods=["GET","POST"])
@@ -96,3 +96,32 @@ def edit(id):
         return redirect(url_for('.post',id=post.id))
     form.body.data=post.body
     return render_template('edit_post.html',form=form)
+@main.route("/follow/<username>")
+@login_required
+@permission_required(Permission.FOLLOW)
+def follow(username):
+    user=User.query.filter_by(username=username).first()
+    if user is None:
+        flash("Invalid user.")
+        return redirect(url_for('.index'))
+    if current_user.is_following(user):
+        flash("You are already following this user.")
+        return redirect(url_for('.user',username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash("You are now following %s." % username)
+    return redirect(url_for('.user',username=username))
+@main.route('/followers/<username>')
+def followers(username):
+    user=User.query.filter_by(username=username).first()
+    if user is None:
+        flash("Invalid user.")
+        return redirect(url_for(".index"))
+    page=request.args.get('page',1,type=int)
+    pagination=user.followers.paginate(
+        page,per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=false)
+    follows=[{'user':item.follower,'timestamp':item.timestamp} for item in pagination.items]
+    return render_template('followers.html',user=user,title="Followers of",
+    endpoint=".followers",pagination=pagination,follows=follows)
+
