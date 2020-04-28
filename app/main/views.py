6bffb4abf384aafebc0a365d8cbdeda230244c9b
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-28 11:53:24 -0700

    probably 12d

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import current_app,Flask,render_template,flash,session,redirect,url_for,request
+from flask import current_app,Flask,render_template,flash,session,redirect,url_for,request,make_response#
 from . import main
 from .forms import NameForm,PostForm
 from .. import db
@@ -52,12 +52,32 @@ def index():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('.index'))
-    posts=Post.query.order_by(Post.timestamp.desc()).all()
+    show_followed=False
+    if current_user.is_authenticated:
+        show_followed=bool(request.cookies.get("show_followed",""))
+    if show_followed:
+        query=current_user.followed_posts
+    else:
+        query=Post.query
     page=request.args.get("page",1,type=int)
     pagination=Post.query.order_by(Post.timestamp.desc()).paginate(
         page,per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],error_out=False)
     posts=pagination.items
-    return render_template('index.html',form=form,posts=posts,pagination=pagination)
+    return render_template('index.html',form=form,posts=posts,show_followed=show_followed,pagination=pagination)
+
+@main.route("/all")
+@login_required
+def show_all():
+    resp=make_response(redirect(url_for(".index")))
+    resp.set_cookies("show_followed","",max_age=30*24*60*60)
+    return resp
+
+@main.route("/followed")
+@login_required
+def show_followed():
+    resp=make_response(redirect(url_for(".index")))
+    resp.set_cookies("show_followed","1",max_age=30*24*60*60)
+    return resp
 
 @main.route('/edit-profile',methods=["GET","POST"])
 @login_required
