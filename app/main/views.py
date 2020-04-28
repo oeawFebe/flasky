e78dd5855655997200e0094e915c563cff9aa45b
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-28 15:08:29 -0700

    end of chapter 13

--- app/main/views.py
+++ app/main/views.py
@@ -9,46 +9,6 @@ from flask_login import login_required, current_user
 from ..decorators import admin_required,permission_required
 from .forms import EditProfileForm, EditProfileAdminForm
 
-@main.route('/edit-profile/<int:id>',methods=["GET","POST"])
-@login_required
-@admin_required
-def edit_profile_admin(id):
-    user=User.query.get_or_404(id)
-    form=EditProfileAdminForm(user=user)
-    if form.validate_on_submit():
-        user.email=form.email.data
-        user.username=form.username.data
-        user.confirmed=form.confirmed.data
-        user.role=Role.query.get(form.role.data)
-        user.name=form.name.data
-        user.location=form.location.data
-        user.about_me=form.about_me.data
-        db.session.add(user)
-        db.session.commit()
-        flash("The profile has been updated.")
-        return redirect(url_for('.user',username=user.username))
-    form.email.data=user.email
-    form.username.data=user.username
-    form.confirmed.data=user.confirmed
-    form.role.data=user.role_id
-    form.name.data=user.location
-    form.about_me.data=user.about_me
-    return render_template("edit_profile.html",form=form,user=user)
-
-
-@main.route("/user/<username>")
-def user(username):
-    user=User.query.filter_by(username=username).first()
-    page=request.args.get("page",1,type=int)
-    pagination=user.posts.order_by(
-        Post.timestamp.desc()
-        ).paginate(
-        page,per_page=current_app.config["FLASKY_POSTS_PER_PAGE"],
-        error_out=False
-        )
-    posts=pagination.items
-    return render_template("user.html",user=user,posts=posts,pagination=pagination)
-
 @main.route("/",methods=["GET","POST"])
 def index():
     form=PostForm()
@@ -77,12 +37,91 @@ def show_all():
     resp.set_cookie("show_followed","",max_age=30*24*60*60)
     return resp
 
+
+@main.route('/edit-profile/<int:id>',methods=["GET","POST"])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user=User.query.get_or_404(id)
+    form=EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email=form.email.data
+        user.username=form.username.data
+        user.confirmed=form.confirmed.data
+        user.role=Role.query.get(form.role.data)
+        user.name=form.name.data
+        user.location=form.location.data
+        user.about_me=form.about_me.data
+        db.session.add(user)
+        db.session.commit()
+        flash("The profile has been updated.")
+        return redirect(url_for('.user',username=user.username))
+    form.email.data=user.email
+    form.username.data=user.username
+    form.confirmed.data=user.confirmed
+    form.role.data=user.role_id
+    form.name.data=user.location
+    form.about_me.data=user.about_me
+    return render_template("edit_profile.html",form=form,user=user)
+
+
+@main.route("/moderate")
+@login_required
+@permission_required(Permission.MODERATE)
+def moderate():
+    page=request.args.get("page",1,type=int)
+    pagination=Comment.query.order_by(
+        Comment.timestamp.desc()
+        ).paginate(
+        page,per_page=current_app.config["FLASKY_COMMENTS_PER_PAGE"],
+        error_out=False
+        )
+    comments=pagination.items
+    return render_template("moderate.html",comments=comments,
+        pagination=pagination,page=page)
+
+@main.route("/moderate/disable/<int:id>")
+@login_required
+@permission_required(Permission.MODERATE)
+def moderate_disable(id):
+    comment=Comment.query.get_or_404(id)
+    comment.diabled=True
+    db.session.add(comment)
+    return redirect(url_for(".moderate",page=request.args.get("page",1,type=int)))
+
+@main.route("/moderate/enable/<int:id>")
+@login_required
+@permission_required(Permission.MODERATE)
+def moderate_enable(id):
+    comment=Comment.query.get_or_404(id)
+    comment.diabled=False
+    db.session.add(comment)
+    return redirect(url_for(".moderate",page=request.args.get("page",1,type=int)))
+
+
 @main.route("/followed")
 @login_required
 def show_followed():
     resp=make_response(redirect(url_for(".index")))
     resp.set_cookie("show_followed","1",max_age=30*24*60*60)
     return resp
+
+
+@main.route("/edit/<int:id>",methods=["GET","POST"])
+@login_required
+def edit(id):
+    post=Post.query.get_or_404(id)
+    if current_user!=post.author and not current_user.can(Permission.ADMIN):
+        abort(403)
+    form=PostForm()
+    if form.validate_on_submit():
+        post.body=form.body.data
+        db.session.add(post)
+        db.session.commit()
+        flash("The post has been updated")
+        return redirect(url_for('.post',id=post.id))
+    form.body.data=post.body
+    return render_template('edit_post.html',form=form)
 
 @main.route('/edit-profile',methods=["GET","POST"])
 @login_required
@@ -101,26 +140,7 @@ def edit_profile():
     form.about_me.data=current_user.about_me
     return render_template('edit_profile.html',form=form)
 
-@main.route("/post/<int:id>")
-def post(id):
-    post=Post.query.get_or_404(id)
-    return render_template('post.html',posts=[post])
 
-@main.route("/edit/<int:id>",methods=["GET","POST"])
-@login_required
-def edit(id):
-    post=Post.query.get_or_404(id)
-    if current_user!=post.author and not current_user.can(Permission.ADMIN):
-        abort(403)
-    form=PostForm()
-    if form.validate_on_submit():
-        post.body=form.body.data
-        db.session.add(post)
-        db.session.commit()
-        flash("The post has been updated")
-        return redirect(url_for('.post',id=post.id))
-    form.body.data=post.body
-    return render_template('edit_post.html',form=form)
 @main.route("/follow/<username>")
 @login_required
 @permission_required(Permission.FOLLOW)
@@ -136,6 +156,20 @@ def follow(username):
     db.session.commit()
     flash("You are now following %s." % username)
     return redirect(url_for('.user',username=username))
+@main.route("/followed_by/<username>")
+def followed_by(username):
+    user=User.query.filter_by(username=username).first()
+    if user is None:
+        flash("Invalid user.")
+        return redirect(url_for(".index"))
+    page=request.args.get("page",1,type=int)
+    pagination=user.followed.paginate(
+        page,per_page=current_app.config["FLASKY_POSTS_PER_PAGE"],
+        error_out=False)
+    follows = [{'user':item.followed,"timestamp":item.timestamp} for item in pagination.items]
+    return render_template("followers.html",user=user,title="Followed by",
+        endpoint=".followed_by",pagination=pagination,follows=follows)
+
 @main.route('/followers/<username>')
 def followers(username):
     user=User.query.filter_by(username=username).first()
@@ -149,6 +183,11 @@ def followers(username):
     follows=[{'user':item.follower,'timestamp':item.timestamp} for item in pagination.items]
     return render_template('followers.html',user=user,title="Followers of",
     endpoint=".followers",pagination=pagination,follows=follows)
+
+@main.route("/post/<int:id>")
+def post(id):
+    post=Post.query.get_or_404(id)
+    return render_template('post.html',posts=[post])
 
 @main.route("/unfollow/<username>")
 @login_required
@@ -165,16 +204,17 @@ def unfollow(username):
     flash("You are not following %s anymore." % username)
     return redirect(url_for(".user",username=username))
 
-@main.route("/followed_by/<username>")
-def followed_by(username):
+
+@main.route("/user/<username>")
+def user(username):
     user=User.query.filter_by(username=username).first()
-    if user is None:
-        flash("Invalid user.")
-        return redirect(url_for(".index"))
     page=request.args.get("page",1,type=int)
-    pagination=user.followed.paginate(
+    pagination=user.posts.order_by(
+        Post.timestamp.desc()
+        ).paginate(
         page,per_page=current_app.config["FLASKY_POSTS_PER_PAGE"],
-        error_out=False)
-    follows = [{'user':item.followed,"timestamp":item.timestamp} for item in pagination.items]
-    return render_template("followers.html",user=user,title="Followed by",
-        endpoint=".followed_by",pagination=pagination,follows=follows)
+        error_out=False
+        )
+    posts=pagination.items
+    return render_template("user.html",user=user,posts=posts,pagination=pagination)
+
