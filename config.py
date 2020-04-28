940296123c41cb6212337405285d8e12cdd436a8
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-27 22:07:09 -0700

    11d FAKER is SO FUN.

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     FLASKY_MAIL_SENDER='FLASKY Admin <flasky@example.com>'
     FLASKY_ADMIN=os.environ.get('FLASKY_ADMIN',"obeythetestinggoat2@gmail.com")#recepient
     SQLALCHEMY_TRACK_MODIFICATIONS=False
-
+    FLASKY_POSTS_PER_PAGE=5
 
     @staticmethod
     def init_app(app):
