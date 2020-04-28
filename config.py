e8f1c5df37e0ea159db57f4e491279e49a6fcbf3
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-28 14:03:22 -0700

    beginning of 13a

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ class Config:
     FLASKY_ADMIN=os.environ.get('FLASKY_ADMIN',"obeythetestinggoat2@gmail.com")#recepient
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     FLASKY_POSTS_PER_PAGE=5
+    FLASKY_COMMENTS_PER_PAGE=3
 
     @staticmethod
     def init_app(app):
