d10cbc9bad3d48176898a3894f9a4a2bc4b858ee
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-27 19:54:57 -0700

    Gravatar is fun.

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from flask_login import UserMixin,AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from datetime import datetime
+import hashlib
+from flask import request
 class Permission:
     FOLLOW=1
     COMMENT=2
@@ -147,7 +149,11 @@ class User(UserMixin, db.Model):
         return self.role is not None and self.role.has_permission(perm)
     def is_administrator(self):
         return self.can(Permission.ADMIN)
-
+    def gravatar(self,size=100,default='identicon',rating='g'):
+        url="https://secure.gravatar.com/avatar"
+        hash=hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url,hash=hash,size=size,default=default,rating=rating)
 
     def __repr__(self):
         return '<User %r>' % self.username
