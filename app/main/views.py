7b5abd1aa9ee415001b331c8fd1343b22096c50d
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-28 13:31:33 -0700

    improved a lot of details

--- app/main/views.py
+++ app/main/views.py
@@ -39,10 +39,15 @@ def edit_profile_admin(id):
 @main.route("/user/<username>")
 def user(username):
     user=User.query.filter_by(username=username).first()
-    if user is None:
-        abort(404)
-    posts=user.posts.order_by(Post.timestamp.desc()).all()
-    return render_template("user.html",user=user,posts=posts)
+    page=request.args.get("page",1,type=int)
+    pagination=user.posts.order_by(
+        Post.timestamp.desc()
+        ).paginate(
+        page,per_page=current_app.config["FLASKY_POSTS_PER_PAGE"],
+        error_out=False
+        )
+    posts=pagination.items
+    return render_template("user.html",user=user,posts=posts,pagination=pagination)
 
 @main.route("/",methods=["GET","POST"])
 def index():
@@ -140,7 +145,7 @@ def followers(username):
     page=request.args.get('page',1,type=int)
     pagination=user.followers.paginate(
         page,per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
-        error_out=false)
+        error_out=False)
     follows=[{'user':item.follower,'timestamp':item.timestamp} for item in pagination.items]
     return render_template('followers.html',user=user,title="Followers of",
     endpoint=".followers",pagination=pagination,follows=follows)
@@ -169,7 +174,7 @@ def followed_by(username):
     page=request.args.get("page",1,type=int)
     pagination=user.followed.paginate(
         page,per_page=current_app.config["FLASKY_POSTS_PER_PAGE"],
-        error_out=Flase)
+        error_out=False)
     follows = [{'user':item.followed,"timestamp":item.timestamp} for item in pagination.items]
     return render_template("followers.html",user=user,title="Followed by",
         endpoint=".followed_by",pagination=pagination,follows=follows)
