7f13092f47f84ede8b44bf4db232205b3c768dd6
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-28 20:58:37 -0700

    Coverage works

--- flasky.py
+++ flasky.py
@@ -1,7 +1,4 @@
-import os,sys,click
-from app import create_app,db
-from app.models import User,Role
-from flask_migrate import Migrate
+import os
 
 COV=None
 if os.environ.get("FLASK_COVERAGE"):
@@ -9,30 +6,44 @@ if os.environ.get("FLASK_COVERAGE"):
     COV=coverage.coverage(branch=True,include="app/*")
     COV.start()
 
+import sys,click
+from app import create_app,db
+from app.models import User,Role,Follow,Permission,Post,Comment
+from flask_migrate import Migrate
+
+
 app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate=Migrate(app,db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db,User=User,Role=Role)
+    return dict(db=db,User=User,Role=Role,Follow=Follow,
+        Permission=Permission,Post=Post,Comment=Comment)
 
 
-@app.cli.command()
-def test():
-    """Run the unittest. """
-    import unittest
-    tests=unittest.TestLoader().discover("tests")
-    unittest.TextTestRunner(verbosity=2).run(tests)
+# @app.cli.command()
+# def test():
+#     """Run the unittest. """
+#     import unittest
+#     tests=unittest.TestLoader().discover("tests")
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 
 @app.cli.command()
 @click.option("--coverage/--no-coverage",default=False,help="Run tests under code coverage.")
-def test(coverage):
+@click.argument("test_names",nargs=-1)
+def test(coverage,test_names):
     """Run the unit tests."""
     if coverage and not os.environ.get("FLASK_COVERAGE"):
+        import subprocess
         os.environ["FLASK_COVERAGE"]="1"
-        os.execvp(sys.executable,[sys.executable]+sys.argv)
+        sys.exit(subprocess.call(sys.argv))
     import unittest
-    tests=unittest.TestLoader().discover("tests")
+    if test_names:
+
+        tests=unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+
+        tests=unittest.TestLoader().discover("tests")
     unittest.TextTestRunner(verbosity=2).run(tests)
     if COV:
         COV.stop()
