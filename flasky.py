73cf711ad63203eb67e431042369fcf2c69c5b0e
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-30 20:06:25 -0700

    17a

--- flasky.py
+++ flasky.py
@@ -9,8 +9,7 @@ if os.environ.get("FLASK_COVERAGE"):
 import sys,click
 from app import create_app,db
 from app.models import User,Role,Follow,Permission,Post,Comment
-from flask_migrate import Migrate
-
+from flask_migrate import Migrate, upgrade
 
 app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate=Migrate(app,db)
@@ -63,3 +62,10 @@ def profile(length,profile_dir):
     app.wsgi_app=ProfilerMiddleware(app.wsgi_app,restrictions=[length],
         profile_dir=profile_dir)
     app.run(debug=False)
+
+@manager.command
+def deploy():
+    upgrade()
+    Role.insert_roles()
+    User.add_self_follows()
+    
