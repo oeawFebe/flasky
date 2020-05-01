7d57cc5076a36e1cd9cda9c2f6c6cda3c0b54407
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-05-01 11:56:42 -0700

    going to deploy to docker

--- boot.sh
+++ boot.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-source virtualenv/bin/activate
-flask deploy
+source venv/bin/activate
+
+while true; do
+    flask deploy
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Deploy command failed, retrying in 5 secs...
+    sleep 5
+done
+
 exec gunicorn -b :5000 --access-logfile - --error-logfile - flasky:app
